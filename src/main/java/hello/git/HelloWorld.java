831a1612c12311d8794f705ac90cf2001a0b18ee
Anna Brown <annabt1901@yahoo.com>
2016-07-25 06:20:18 +0100

    New: Users can get their name printed

--- src/main/java/hello/git/HelloWorld.java
+++ src/main/java/hello/git/HelloWorld.java
@@ -2,6 +2,6 @@ package hello.git;
 
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("Hello World! " + args[0]);
   }
 }
