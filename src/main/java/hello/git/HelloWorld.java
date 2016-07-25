64a6c140f56fb323712253910357a11c58bc7ea7
Anna Brown <annabt1901@yahoo.com>
2016-07-25 06:29:28 +0100

    Fix: Add some checking

--- src/main/java/hello/git/HelloWorld.java
+++ src/main/java/hello/git/HelloWorld.java
@@ -2,6 +2,11 @@ package hello.git;
 
 public class HelloWorld {
   public static void main(String[] args) {
+    if (args.length == 0) {
+      System.err.println("Required one argument");
+      System.exit(1);
+    }
+
     System.out.println("Hello World! " + args[0]);
   }
 }
